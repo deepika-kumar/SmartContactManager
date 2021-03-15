7a798cdf124c90e90ae7727523332838585391a5
Priti <Deepika>
2021-03-14 21:34:31 -0500

    Signup page

--- smartcontactmanager/src/main/java/com/smart/entities/User.java
+++ smartcontactmanager/src/main/java/com/smart/entities/User.java
@@ -87,6 +87,12 @@ public class User {
 	public void setContacts(List<Contact> contacts) {
 		this.contacts = contacts;
 	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + ", role=" + role
+				+ ", enabled=" + enabled + ", imageURL=" + imageURL + ", about=" + about + ", contacts=" + contacts
+				+ "]";
+	}
 	
 	
 	
